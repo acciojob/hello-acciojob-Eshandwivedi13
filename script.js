47e807f0425c2f9358853bbeb9b36261b619e157
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-18 01:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+it('should have correct styles', () => {
+  cy.visit('your_page_url'); // Replace with your actual page URL
+  cy.get('main.accio#job', { timeout: 10000 })
+    .should('exist')
+    .should('be.visible')
+    .should('have.css', 'width', '150px')
+    .should('have.css', 'height', '150px')
+    .should('have.css', 'background-color', 'rgb(0, 0, 0)');
+});
